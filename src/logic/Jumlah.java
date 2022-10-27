0ed619c2034d07f83d0f5b2a1232e6c283d922f7
Ni Wayan Amanda Putri Astawa <99030230+jstmnd@users.noreply.github.com>
2022-10-27 15:46:12 +0800

    Update Jumlah.java

--- src/logic/Jumlah.java
+++ src/logic/Jumlah.java
@@ -1,5 +1,29 @@
 package logic;
 
 public class Jumlah {
+    public double divide;
     
+
+    public Jumlah (double x, double y){
+        this.divide = proses(x, y);
+    }
+
+    public Jumlah (int x, int y){
+        this.divide = proses(x, y);
+    }
+    public Jumlah (String x){
+        this.divide = proses(x);
+    }
+
+    double proses (int x, int y){
+        return (double) (x+y);
+    }
+
+    double proses (double x, double y){
+        return x+y;
+    }
+    double proses (String x){
+        var arr = x.split(" ");
+        return Double.parseDouble(arr[0]) + Double.parseDouble(arr[1]);
+    }
 }
